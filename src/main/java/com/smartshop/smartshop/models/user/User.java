256bf73f4d0a93fc9b5862ddaa7aa0d325fd7d63
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-24 16:42:56 +0100

    feat: add user model

---
+++ src/main/java/com/smartshop/smartshop/models/user/User.java
@@ -0,0 +1,38 @@
+package com.smartshop.smartshop.models.user;
+
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserRole role;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+}
+
---
+++ src/main/java/com/smartshop/smartshop/models/user/UserRole.java
@@ -0,0 +1,8 @@
+package com.smartshop.smartshop.models.user;
+
+
+public enum UserRole {
+    ADMIN,
+    CLIENT
+}
+
