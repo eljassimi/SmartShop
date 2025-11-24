c1a5510372642a89f340e7b3e53db41febf27a04
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-24 16:50:49 +0100

    feat: add user repository

---
+++ src/main/java/com/smartshop/smartshop/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.smartshop.smartshop.repositories;
+
+import com.smartshop.smartshop.models.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
+
