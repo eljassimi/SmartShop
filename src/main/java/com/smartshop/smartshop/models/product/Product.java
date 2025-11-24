ce9769d17a0eec8ea563636dcfe5632325397f66
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-24 16:43:30 +0100

    feat: add product model

---
+++ src/main/java/com/smartshop/smartshop/models/product/Product.java
@@ -0,0 +1,51 @@
+package com.smartshop.smartshop.models.product;
+
+import com.smartshop.smartshop.models.order.OrderItem;
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String sku;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "unit_price", nullable = false)
+    private double unitPrice;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @OneToMany(mappedBy = "product")
+    private List<OrderItem> orderItems;
+}
+
