8629ac0e0a003d90055aa319ad03d8b0b2771268
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-27 09:45:21 +0100

    feat: Exceptions

---
+++ src/main/java/com/smartshop/smartshop/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.smartshop.smartshop.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/com/smartshop/smartshop/exceptions/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.smartshop.smartshop.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
