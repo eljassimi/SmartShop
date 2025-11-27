ad757361ba94580c88d63036351850f7f13bff4d
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-27 11:43:20 +0100

    feat: product mapper

---
+++ src/main/java/com/smartshop/smartshop/mappers/ProductMapper.java
@@ -0,0 +1,35 @@
+package com.smartshop.smartshop.mappers;
+
+import com.smartshop.smartshop.dto.product.ProductRequest;
+import com.smartshop.smartshop.dto.product.ProductResponse;
+import com.smartshop.smartshop.models.product.Product;
+import java.util.List;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductResponse toResponse(Product product);
+
+    List<ProductResponse> toResponseList(List<Product> products);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "orderItems", ignore = true)
+    Product toEntity(ProductRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "orderItems", ignore = true)
+    void updateEntityFromRequest(ProductRequest request, @MappingTarget Product product);
+}
+
